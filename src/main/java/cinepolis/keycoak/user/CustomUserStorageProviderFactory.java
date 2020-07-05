150355025172e4f3485412be1b6e3c6041885804
root <arellano.gustavo@gmail.com>
2020-07-05 18:34:50 -0500

    try 001

--- src/main/java/cinepolis/keycoak/user/CustomUserStorageProviderFactory.java
+++ src/main/java/cinepolis/keycoak/user/CustomUserStorageProviderFactory.java
@@ -8,7 +8,7 @@ import org.keycloak.storage.UserStorageProviderFactory;
 
 import java.util.List;
 
-public class DemoUserStorageProviderFactory implements UserStorageProviderFactory<CustomUserStorageProvider> {
+public class CustomUserStorageProviderFactory implements UserStorageProviderFactory<CustomUserStorageProvider> {
 
     @Override
     public CustomUserStorageProvider create(KeycloakSession session, ComponentModel model) {
